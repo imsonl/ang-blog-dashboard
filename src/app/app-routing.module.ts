cc7b8ba38c34d222c0346cb7536d51b14e293421
imsonal <sonal.patel@archesoftronix.com>
2023-11-10 11:17:11 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'',component:HomeComponent},
+  {path:'category',component:SingleCategoryComponent},
+  {path:'post',component:SinglePostComponent},
+
+  {path:'about',component:AboutUsComponent},
+  {path:'terms-condition',component:TermsAndConditionComponent},
+  {path:'contact',component:ContactComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
